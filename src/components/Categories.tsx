3b1ab8abaf73f94200acebb6ea20337d05510d18
OlegikSSoN <olegiksson@gmail.com>
2023-10-08 23:03:52 +0300

    Pizza

---
+++ src/components/Categories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+type CategoriesProps = {
+  value: number;
+  onChangeCategory: (idx: number) => void;
+};
+
+const categories = [
+  "Всі",
+  "М'ясні",
+  "Вегетеріанські",
+  "Гриль",
+  "Гострі",
+  "Закриті",
+];
+
+export const Categories: React.FC<CategoriesProps> = React.memo(
+  ({ value, onChangeCategory }) => {
+    return (
+      <div className="categories">
+        <ul>
+          {categories.map((categoryName, i) => (
+            <li
+              key={i}
+              onClick={() => onChangeCategory(i)}
+              className={value === i ? "active" : ""}
+            >
+              {categoryName}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+);
